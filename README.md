a2f3daac216c8bc55f0976eeb49ae0d762ef0af8
Donald Nevtis <nevtees@gmail.com>
2019-12-12 22:41:39 +0300

    gitignore changes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# culc
+# Crypton
 
 ## Project setup
 ```
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-    devServer: {
-        disableHostCheck: true
-    },
-    publicPath: process.env.NODE_ENV === 'production' ?
-        '/Crypton/' : '/'
+    // devServer: {
+    //     disableHostCheck: true
+    // },   
 }
