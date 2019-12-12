3cebbd399a092553b48bf788c223a31bd6ae0e2c
Donald Nevtis <nevtees@gmail.com>
2019-12-12 21:24:13 +0300

    deploy settings

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-      devServer: {
+    devServer: {
         disableHostCheck: true
-    }
+    },
+    publicPath: process.env.NODE_ENV === 'production' ?
+        '/my-project/' :
+        '/'
 }
