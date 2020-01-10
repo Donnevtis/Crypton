3ba03fbe124a4571bfe22f176b9bc791b5386aad
Donald Nevtis <nevtees@gmail.com>
2020-01-10 18:20:59 +0300

    migration to axios

--- src/api/coincap-api.js
+++ src/api/coincap-api.js
@@ -1,3 +1,5 @@
+const axios = require('axios').default;
+
 export default {
     fullData(coinName) {
         const link = `https://api.coincap.io/v2/assets/${coinName}/history?interval=d1`
@@ -9,19 +11,15 @@ export default {
         return this.fetch(link)
     },
     fetch(link) {
-        return new Promise(async (resolve, reject) => {
-            let res = await fetch(link)
-            if (!(res.ok)) {
-                reject(res.status)
-                return
-            }
-            res = await res.json()
-            res.data.sort((a, b) => a.time - b.time)
-            resolve(res.data)
-        })
+        return axios.get(link)
+            .then(res => {
+                if (res.status !== 200) throw Error('Data is not receive') // Needs to write error handler
+                return res.data
+            })
+            .then(res => res.data.sort((a, b) => a.time - b.time))
     },
     openWS(coinName, callback) {
-        // Need changes. It will be rather, if WebSocket won't be closes.       
+        // Needs changes. It will be rather, if WebSocket won't be closes.       
         this.WS = new WebSocket(`wss://ws.coincap.io/prices?assets=${coinName}`)
         const startTime = Date.now();
         let time = 0;
