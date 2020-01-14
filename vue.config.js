a72c6ca4ff9a32c6f035da64d1dd9f4f3050ff13
Donald Nevtis <nevtees@gmail.com>
2020-01-14 12:38:39 +0300

    yarn added

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,15 @@
+const path = require('path')
 module.exports = {
     devServer: {
         disableHostCheck: true
     },
     publicPath: process.env.NODE_ENV === 'production' ?
-        '/Crypton/' : '/'
+        '/Crypton' : '/',
+    configureWebpack: {
+        resolve: {
+            alias: {
+                'utils': path.resolve(__dirname, 'src/utils')
+            }
+        }
+    }
 }
