12c6c20ad0d50b101e7d63a4fbfc59972ced19df
Donald Nevtis <nevtees@gmail.com>
2020-02-03 22:27:38 +0300

    vue.config delete

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-const path = require('path')
-module.exports = {
-    devServer: {
-        disableHostCheck: true
-    },
-    publicPath: process.env.NODE_ENV === 'production' ?
-        '/' : '/'
-}
