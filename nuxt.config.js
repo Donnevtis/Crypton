b2cb3240153a715ede8f91d114db064ed00beced
Donald Nevtis <nevtees@gmail.com>
2020-02-04 09:16:24 +0300

    upgrade packages

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,5 +56,21 @@ export default {
         scss: [
             './assets/scss/_common.scss'
         ]
+    },
+    build: {
+        babel: {
+            presets({ isServer }) {
+                return [
+                    [
+                        require.resolve('@nuxt/babel-preset-app'),
+                        // require.resolve('@nuxt/babel-preset-app-edge'), // For nuxt-edge users
+                        {
+                            buildTarget: isServer ? 'server' : 'client',
+                            corejs: { version: 3 }
+                        }
+                    ]
+                ]
+            }
+        }
     }
 }
